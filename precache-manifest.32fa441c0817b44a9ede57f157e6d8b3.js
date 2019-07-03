11c6647f245cf7f86985cafaeeee74ae411fa01f
lelethungalo@gmail.com <leletungalo@gmail.com>
2019-07-04 00:00:23 +0200

    Updates

---
+++ precache-manifest.32fa441c0817b44a9ede57f157e6d8b3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d386547f20a07e65a353cf9b7397c07",
+    "url": "/ZaioDisplayName/index.html"
+  },
+  {
+    "revision": "4e615afe0a3d230d35e5",
+    "url": "/ZaioDisplayName/static/css/main.c0523444.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/ZaioDisplayName/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "4e615afe0a3d230d35e5",
+    "url": "/ZaioDisplayName/static/js/main.6779c660.chunk.js"
+  },
+  {
+    "revision": "e40add905a3152405f7a",
+    "url": "/ZaioDisplayName/static/js/runtime~main.335efe52.js"
+  }
+]);
